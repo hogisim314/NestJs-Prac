747b990b8e1ae406f75127e800dc97210d2d6a7b
심기호 <giho@simgihoui-MacBookAir.local>
2023-08-05 15:45:16 +0900

    user.entity comment추가

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -13,6 +13,6 @@ export class User extends BaseEntity {
     @Column()
     username : string;
 
-    @OneToMany(type => Board, board => board.user, {eager:true})
+    @OneToMany(type => Board, board => board.user, {eager:true})//type은 그 column의 타입, board->는 board를 접근할 때 어떻게 접근할 것인지..(board entity의 user로 접근), eager는 user를 부르면 board가 따라서 불러지냐 라는 말.
     boards:Board[]
 }
